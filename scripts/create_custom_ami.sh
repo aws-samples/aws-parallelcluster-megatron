64105ed636f2954edadbbc8c2fd98eee9731974c
Maxime Hugues <maxhaws@amazon.com>
2021-03-02 21:00:07 +0000

    Improve/tutorial steps

--- scripts/create_custom_ami.sh
+++ scripts/create_custom_ami.sh
@@ -1,17 +1,19 @@
-#!/bin/bash 
+#!/bin/bash
 #
 #
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 #
 #
+
+echo "Deep Learning AMI ID $DEEP_LEARNING_AMI_ID"
+
 echo "Creating a new AMI"
 
-pcluster createami -ai ami-01a495658aa5f7930 \
+pcluster createami -ai $DEEP_LEARNING_AMI_ID \
 	-os alinux2 \
 	-ap megatron-on-pcluster- \
 	-c $(pwd)/configs/base-config-build-ami.ini \
 	-i p4d.24xlarge \
 	--post-install file://$(pwd)/scripts/custom_dlami_user_data.sh \
-	-r us-west-2
-
+	-r ${AWS_REGION}
